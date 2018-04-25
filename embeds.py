ef4ebe093f54d61f6da533ae65dc2a49ee2e19a4
eloibg <eloi.brassard-gourdeau.1@ulaval.ca>
2018-04-25 09:52:20 -0400

    refactor nom

--- embeds.py
+++ embeds.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import csv
-import numpy as np
 
 glove_data_file = "C:\\Users\\eloib\\Downloads\\glove.twitter.27B\\glove.twitter.27B.200d.txt"
 
@@ -8,13 +7,3 @@ words = pd.read_table(glove_data_file, sep=" ", index_col=0, header=None, quotin
 
 def vec(w):
   return words.loc[w].as_matrix()
-
-words_matrix = words.as_matrix()
-
-def find_closest_word(v):
-  diff = words_matrix - v
-  delta = np.sum(diff * diff, axis=1)
-  i = np.argmin(delta)
-  return words.iloc[i].name
-
-print(find_closest_word("hey"))
