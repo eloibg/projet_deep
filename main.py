b2555adbe461b612122f096dd11549961144b4b8
nick3185 <nick3185@gmail.com>
2018-04-25 11:55:15 -0400

    refactoring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import torch
+from string import ascii_lowercase
 import torch.nn as nn
 
 
@@ -13,10 +13,6 @@ class Language:
         self.index_to_word = {BOW_token: "BOW", EOW_token: "EOW"}
         self.n_words = 2
 
-    def add_sentence(self, sentence):
-        for word in sentence.split(' '):
-            self.add_word(word)
-
     def add_word(self, word):
         if word not in self.word_to_index:
             self.word_to_index[word] = self.n_words
@@ -25,6 +21,29 @@ class Language:
             self.n_words += 1
         else:
             self.word_to_count[word] += 1
+
+
+class WordLevelEncoder(Language):
+    def __init__(self):
+        super(WordLevelEncoder, self).__init__()
+
+    def add_sentence(self, sentence):
+        for word in sentence.split(' '):
+            self.add_word(word)
+
+
+class CharacterLevelEncoder(Language):
+    def __init__(self):
+        super(CharacterLevelEncoder, self).__init__()
+        for c in ascii_lowercase:
+            self.word_to_index[c] = self.n_words
+            self.word_to_count[c] = 1
+            self.index_to_word[self.n_words] = c
+            self.n_words += 1
+
+    def add_sentence(self, sentence):
+        for word in list(sentence):
+            self.add_word(word)
 
     def make_one_hot(self, word):
         if word in self.word_to_index:
@@ -60,9 +79,9 @@ if __name__ == '__main__':
     premiere_phrase = "une phrase quelconque"
     deuxieme_phrase = "une autre phrase pas rapport"
 
-    lang = Language()
+    lang = CharacterLevelEncoder()
     lang.add_sentence(premiere_phrase)
     lang.add_sentence(deuxieme_phrase)
     print(lang.n_words)
     print(lang.index_to_word)
-    print(lang.make_one_hot("phrase"))
+    print(lang.make_one_hot("p"))
