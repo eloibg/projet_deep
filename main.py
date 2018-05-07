517ed23b362b3c687f9394a48ffd30b9f884a520
nick <nick3185@gmail.com>
2018-05-07 14:51:38 -0400

    test

--- main.py
+++ main.py
@@ -51,25 +51,20 @@ class CharacterLevelEncoder(Language):
 
 
 class CNN(nn.Module):
-    def __init__(self):
+    def __init__(self, n_layers=50):
         super(CNN, self).__init__()
 
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, padding=2),
-            nn.ReLU())
-
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
-
-        self.fc = nn.Linear(7 * 7 * 32, 10)
+        self.layers = []
+        for i in range(n_layers):
+            layer = nn.Sequential(nn.Conv2d(1, 16, kernel_size=3, padding=1),
+                                  nn.ReLU(),
+                                  nn.AdaptiveAvgPooling1D(1))
+            self.layers.append(layer)
 
     def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
+        out = x
+        for layer in self.layers:
+            out = layer.forward(out)
         return out
 
 
